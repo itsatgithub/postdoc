13588c043210eb6384bc5a03a6e1c40ff67159e1
bartolome@irbbarcelona.org <bartolome@irbbarcelona.org@c4954c31-e19f-eb7c-d949-03f297df9cdb>
2014-11-27 12:24:23 +0000

    wip

--- trunk/components/com_phd/controllers/applicant.php
+++ trunk/components/com_phd/controllers/applicant.php
@@ -1091,15 +1091,14 @@ class PhdControllerApplicant extends JController
 			default: $ctype="application/force-download";
 		}
 	
-		//LOG all downloads
-		$user 	=& JFactory::getUser();
+		// log all downloads
+		$user =& JFactory::getUser();
 		$options = array('format' => "{DATE}\t{TIME}\t{IP}\t{NAME}\t{FILENAME}\t{APPLICANT}");
 		$ip_address = $_SERVER['REMOTE_ADDR'];
 		$log_filename= "file_access-".date( 'M-Y').".log";
 		$log = & JLog::getInstance($log_filename, $options);
-		//$log->addEntry(array("Date" => date('d-m-Y'),"Time" => date('h:i'),"IP" => $ip_address,"Name"=>$user->name,"Filename"=>$filename,"Applicant"=>$applicant->lastname.', '.$applicant->firstname));
 		$log->addEntry(array("Date" => date('d-m-Y'),"Time" => date('h:i'),"IP" => $ip_address,"Name"=>$user->name,"Filename"=>$filename,"Applicant"=>$applicant->firstname.' '.$applicant->lastname));
-		//END LOG
+		// end log
 	
 		//Begin writing headers
 		header("Pragma: public");
@@ -1188,14 +1187,14 @@ class PhdControllerApplicant extends JController
 
 		$z->close();
 
-		//LOG all logins
-		$user 	=& JFactory::getUser();
+		// log all downloads
+		$user =& JFactory::getUser();
 		$options = array('format' => "{DATE}\t{TIME}\t{IP}\t{NAME}\t{FILENAME}\t{APPLICANT}");
 		$ip_address = $_SERVER['REMOTE_ADDR'];
 		$log_filename= "file_access-".date( 'M-Y').".log";
 		$log = & JLog::getInstance($log_filename, $options);
 		$log->addEntry(array("Date" => date('d-m-Y'),"Time" => date('h:i'),"IP" => $ip_address,"Name"=>$user->name,"Filename"=>$filename,"Applicant"=>$applicant->lastname.', '.$applicant->firstname));
-		//END LOG
+		// end log
 
 		$log_zip->addEntry(array('comment' =>'Filename: '.$filename));
 				$log_zip->addEntry(array('comment' =>'Filesize: '.filesize($outZipPath)));
